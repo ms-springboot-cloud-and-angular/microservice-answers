f9c2386dbaedb316dc82350826c0c42e2471b4ce
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-13 12:26:12 +0200

    New SpringBoot project - Answers

---
+++ src/main/java/com/joseluisestevez/ms/app/answers/MicroserviceAnswersApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.answers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAnswersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAnswersApplication.class, args);
+    }
+
+}
